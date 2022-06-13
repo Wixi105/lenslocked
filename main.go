24443409638dbbd289bb2bfd9a209ba5e3e9fbce
wixi105 <ericsampson105@gmail.com>
2022-06-13 15:58:39 +0000

    added chi router

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,23 +14,28 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	fmt.Fprint(w, "<h1> Contact me</h1> <p>To get in touch, email me at ericsampson@gmail.com</p>")
+	fmt.Fprint(w, "<h1> Contact me</h1> <p>To get in touch, email me at <a href=\"mailto:ericsampson109@gmail.com\">ericsampson@gmail.com</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w,"Invalid URL", http.StatusNotFound)
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Frequently Asked Questions</h1>
+	
+	<ul>
+		<li>Can we get a Free Trial? Yes, free trials are available</li>
+		<li>How many people can signup for this event? 300 people per ticket</li>
+		<li>Is this difficult? No, not at all. If you do your best, you will be well rewarded.</li>
+	</ul>
+	
+	`)
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/contact", contactHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+
 	fmt.Println("Starting server on port: 3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
