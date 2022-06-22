cab75c9de2886810004360de8692f81e3e6aa1e4
wixi105 <ericsampson105@gmail.com>
2022-06-22 18:10:22 +0000

    changed router to chi router
    Signed-off-by: wixi105 <ericsampson105@gmail.com>

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,11 +31,35 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
+func articleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	articleByID := chi.URLParam(r, "id")
+
+	switch articleByID {
+	case "1":
+		fmt.Fprintf(w, `
+		<h1>This is the first article</h1>
+		<p>Article %v</p>
+		`, articleByID)
+	case "2":
+		fmt.Fprintf(w, `
+			<h1>This is the second article</h1>
+			<p>Article %v</p>
+			`, articleByID)
+	}
+
+}
+
 func main() {
+
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/article/{id}", articleHandler)
 
 	fmt.Println("Starting server on port: 3000")
 	http.ListenAndServe(":3000", r)
